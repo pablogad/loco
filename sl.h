22ed6629d3ef2692f252e40d2b12641881af233a
Pablo GAD <pablogad@gmail.com>
2017-02-08 08:21:24 +0100

    Create sl.h

---
+++ sl.h
@@ -0,0 +1,145 @@
+/*========================================
+ *    sl.h: Text data of SL version 3.01
+ *	Copyright 1993 Toyoda Masashi
+ *		(toyoda@is.titech.ac.jp)
+ *	Last Modified: 1992/12/23
+ *========================================
+ */
+
+#define D51HIGHT	10
+#define D51FUNNEL	 7
+#define D51LENGTH       83
+#define D51PATTERNS	 6
+
+
+#define D51STR1  "      ====        ________                ___________ "
+#define D51STR2  "  _D _|  |_______/        \\__I_I_____===__|_________| "
+#define D51STR3  "   |(_)---  |   H\\________/ |   |        =|___ ___|   "
+#define D51STR4  "   /     |  |   H  |  |     |   |         ||_| |_||   "
+#define D51STR5  "  |      |  |   H  |__--------------------| [___] |   "
+#define D51STR6  "  | ________|___H__/__|_____/[][]~\\_______|       |   "
+#define D51STR7  "  |/ |   |-----------I_____I [][] []  D   |=======|__ "
+
+#define D51WHL11 "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ "
+#define D51WHL12 " |/-=|___|=    ||    ||    ||    |_____/~\\___/        "
+#define D51WHL13 "  \\_/      \\O=====O=====O=====O_/      \\_/            "
+
+#define D51WHL21 "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ "
+#define D51WHL22 " |/-=|___|=O=====O=====O=====O   |_____/~\\___/        "
+#define D51WHL23 "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            "
+
+#define D51WHL31 "__/ =| o |=-O=====O=====O=====O \\ ____Y___________|__ "
+#define D51WHL32 " |/-=|___|=    ||    ||    ||    |_____/~\\___/        "
+#define D51WHL33 "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            "
+
+#define D51WHL41 "__/ =| o |=-~O=====O=====O=====O\\ ____Y___________|__ "
+#define D51WHL42 " |/-=|___|=    ||    ||    ||    |_____/~\\___/        "
+#define D51WHL43 "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            "
+
+#define D51WHL51 "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ "
+#define D51WHL52 " |/-=|___|=   O=====O=====O=====O|_____/~\\___/        "
+#define D51WHL53 "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            "
+
+#define D51WHL61 "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ "
+#define D51WHL62 " |/-=|___|=    ||    ||    ||    |_____/~\\___/        "
+#define D51WHL63 "  \\_/      \\_O=====O=====O=====O/      \\_/            "
+
+#define D51DEL   "                                                      "
+
+#define COAL01 "                              "
+#define COAL02 "                              "
+#define COAL03 "    _________________         "
+#define COAL04 "   _|                \\_____A "
+#define COAL05 " =|        PGAD            |  "
+#define COAL06 " -|      141-001           |  "
+#define COAL07 "__|________________________|_ "
+#define COAL08 "|__________________________|_ "
+#define COAL09 "   |_D__D__D_|  |_D__D__D_|   "
+#define COAL10 "    \\_/   \\_/    \\_/   \\_/    "
+
+#define COALDEL "                              "
+
+/* BEGIN PGAD */
+#define VAGON11  " ________________________________  "
+#define VAGON12  " |______________________________|  "
+#define VAGON13  "   |    __  __  __  __  __    |    "
+#define VAGON14  "   |   |  ||  ||  ||  ||  |   |    "
+#define VAGON15  "   |   |__||__||__||__||__|   |    "
+#define VAGON16  "   |                          |    "
+#define VAGON17  "_ _|__________________________|_ _ "
+#define VAGON18  "_|______________________________|_ "
+#define VAGON19  "    |_O_|   \\_X_X_X_X_/   |_O_|    "
+#define VAGON110 "     \\_/                   \\_/    "
+#define VAGON1DEL "                                    "
+
+#define VAGON1_HGHT    10
+#define VAGON1_LEN     35
+
+#define VAGON21  " ______o_________o__________o____   "
+#define VAGON22  " |______________________________|   "
+#define VAGON23  "   |    __  __  __  __  __    |     "
+#define VAGON24  "   |   |  ||  ||  ||  ||  |   |     "
+#define VAGON25  "   |   |__||__||__||__||__|   |  |  "
+#define VAGON26  "   |                   | X|   |  |  "
+#define VAGON27  "_ _|___________________|__|___|__|_ "
+#define VAGON28  "_|______________________________ |_ "
+#define VAGON29  "    |_O_|   \\_X_X_X_X_/   |_O_|    "
+#define VAGON210 "     \\_/                   \\_/    "
+#define VAGON2DEL "                                    "
+
+#define VAGON2_HGHT    10
+#define VAGON2_LEN     36
+
+
+/* END PGAD */
+
+#define LOGOHIGHT    	 6
+#define LOGOFUNNEL  	 4
+#define LOGOLENGTH      84
+#define LOGOPATTERNS	 6
+
+#define LOGO1  "     ++      +------ "
+#define LOGO2  "     ||      |+-+ |  "
+#define LOGO3  "   /---------|| | |  "
+#define LOGO4  "  + ========  +-+ |  "
+
+#define LWHL11 " _|--O========O~\\-+  "
+#define LWHL12 "//// \\_/      \\_/    "
+
+#define LWHL21 " _|--/O========O\\-+  "
+#define LWHL22 "//// \\_/      \\_/    "
+
+#define LWHL31 " _|--/~O========O-+  "
+#define LWHL32 "//// \\_/      \\_/    "
+
+#define LWHL41 " _|--/~\\------/~\\-+  "
+#define LWHL42 "//// \\_O========O    "
+
+#define LWHL51 " _|--/~\\------/~\\-+  "
+#define LWHL52 "//// \\O========O/    "
+
+#define LWHL61 " _|--/~\\------/~\\-+  "
+#define LWHL62 "//// O========O_/    "
+
+#define LCOAL1 "____                 "
+#define LCOAL2 "|   \\@@@@@@@@@@@     "
+#define LCOAL3 "|    \\@@@@@@@@@@@@@_ "
+#define LCOAL4 "|                  | "
+#define LCOAL5 "|__________________| "
+#define LCOAL6 "   (O)       (O)     "
+
+#define LCAR1  "____________________ "
+#define LCAR2  "|  ___ ___ ___ ___ | "
+#define LCAR3  "|  |_| |_| |_| |_| | "
+#define LCAR4  "|__________________| "
+#define LCAR5  "|__________________| "
+#define LCAR6  "   (O)        (O)    "
+
+#define DELLN  "                     "
+
+void add_track(int y);
+int add_D51(int x, int longitud_tren);
+int add_vagon(int x, int num_vag, int longitud_tren);
+int add_sl(int x);
+void add_man(int y, int x);
+void add_smoke(int y,int x);
